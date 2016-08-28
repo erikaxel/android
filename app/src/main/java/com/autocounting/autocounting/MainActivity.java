475f698ffec8f3c6531105f29d5c0c43cc5242ba
Erik Axel Nielsen <erikaxel@nielsen-og-co.no>
2016-08-28 02:23:01 +0200

    Initial commit

---
+++ app/src/main/java/com/autocounting/autocounting/MainActivity.java
@@ -0,0 +1,300 @@
+package com.autocounting.autocounting;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.basecamp.turbolinks.TurbolinksSession;
+import com.basecamp.turbolinks.TurbolinksAdapter;
+import com.basecamp.turbolinks.TurbolinksView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity implements TurbolinksAdapter {
+    // Change the BASE_URL to an address that your VM or device can hit.
+//    private static final String BASE_URL = "https://beta.autocounting.no";
+    private static final String BASE_URL = "http://192.168.1.107:3000/";
+    private static final String INTENT_URL = "intentUrl";
+
+    private String location;
+    private TurbolinksView turbolinksView;
+
+    // -----------------------------------------------------------------------
+    // Activity overrides
+    // -----------------------------------------------------------------------
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Find the custom TurbolinksView object in your layout
+        turbolinksView = (TurbolinksView) findViewById(R.id.turbolinks_view);
+
+        // For this demo app, we force debug logging on. You will only want to do
+        // this for debug builds of your app (it is off by default)
+        TurbolinksSession.getDefault(this).setDebugLoggingEnabled(true);
+
+        // For this example we set a default location, unless one is passed in through an intent
+        location = getIntent().getStringExtra(INTENT_URL) != null ? getIntent().getStringExtra(INTENT_URL) : BASE_URL;
+
+        // Execute the visit
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .view(turbolinksView)
+                .visit(location);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // Since the webView is shared between activities, we need to tell Turbolinks
+        // to load the location from the previous activity upon restarting
+        TurbolinksSession.getDefault(this)
+                .activity(this)
+                .adapter(this)
+                .restoreWithCachedSnapshot(true)
+                .view(turbolinksView)
+                .visit(location);
+    }
+
+    // -----------------------------------------------------------------------
+    // TurbolinksAdapter interface
+    // -----------------------------------------------------------------------
+
+    @Override
+    public void onPageFinished() {
+
+    }
+
+    @Override
+    public void onReceivedError(int errorCode) {
+        handleError(errorCode);
+    }
+
+    @Override
+    public void pageInvalidated() {
+
+    }
+
+    @Override
+    public void requestFailedWithStatusCode(int statusCode) {
+        handleError(statusCode);
+    }
+
+    @Override
+    public void visitCompleted() {
+
+    }
+
+    // The starting point for any href clicked inside a Turbolinks enabled site. In a simple case
+    // you can just open another activity, or in more complex cases, this would be a good spot for
+    // routing logic to take you to the right place within your app.
+    @Override
+    public void visitProposedToLocationWithAction(String location, String action) {
+//        http://192.168.1.107:3000/take_picture
+        try {
+            URL url = new URL(location);
+            Log.d("visitProposedToLocation", "location:" + location + " action:" + action + " path:" + url.getPath());
+
+            if(url.getPath().equals("/take_picture")) {
+                dispatchTakePictureIntent();
+            } else {
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.putExtra(INTENT_URL, location);
+                this.startActivity(intent);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    String mCurrentPhotoPath;
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
+//    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_TAKE_PHOTO = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+        // Ensure that there's a camera activity to handle the intent
+//        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+//            // Create the File where the photo should go
+//            File photoFile = null;
+//            try {
+//                photoFile = createImageFile();
+//            } catch (IOException ex) {
+//                // Error occurred while creating the File
+//            }
+//            // Continue only if the File was successfully created
+//            if (photoFile != null) {
+//                Uri photoURI = FileProvider.getUriForFile(this,
+//                        "com.example.android.fileprovider",
+//                        photoFile);
+//                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+//            }
+//        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==REQUEST_TAKE_PHOTO)
+        {
+            Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            thumbnail.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+//            File destination = new File(Environment.getExternalStorageDirectory(),"temp.jpg");
+            try {
+                File destination = createImageFile();
+                FileOutputStream fo;
+                fo = new FileOutputStream(destination);
+                fo.write(bytes.toByteArray());
+                fo.close();
+                new uploadFileToServerTask().execute(destination.getAbsolutePath());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private class uploadFileToServerTask extends AsyncTask<String, String, Object> {
+        @Override
+        protected String doInBackground(String... args) {
+            try {
+                String lineEnd = "\r\n";
+                String twoHyphens = "--";
+                String boundary = "*****";
+                int bytesRead, bytesAvailable, bufferSize;
+                byte[] buffer;
+                @SuppressWarnings("PointlessArithmeticExpression")
+                int maxBufferSize = 1 * 1024 * 1024;
+
+
+//                java.net.URL url = new URL((ApplicationConstant.UPLOAD_IMAGE_URL) + IMAGE + customer_id);
+                java.net.URL url = new URL(BASE_URL + "receipts");
+//                Log.d(ApplicationConstant.TAG, "url " + url);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Allow Inputs &amp; Outputs.
+                connection.setDoInput(true);
+                connection.setDoOutput(true);
+                connection.setUseCaches(false);
+
+                // Set HTTP method to POST.
+                connection.setRequestMethod("POST");
+
+                connection.setRequestProperty("Connection", "Keep-Alive");
+                connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+                FileInputStream fileInputStream;
+                DataOutputStream outputStream;
+                {
+                    outputStream = new DataOutputStream(connection.getOutputStream());
+
+                    outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                    String filename = args[0];
+                    outputStream.writeBytes("Content-Disposition: form-data; name=\"receipt[picture]\";filename=\"" + filename + "\"" + lineEnd);
+                    outputStream.writeBytes(lineEnd);
+//                    Log.d(ApplicationConstant.TAG, "filename " + filename);
+
+                    fileInputStream = new FileInputStream(filename);
+
+                    bytesAvailable = fileInputStream.available();
+                    bufferSize = Math.min(bytesAvailable, maxBufferSize);
+
+                    buffer = new byte[bufferSize];
+
+                    // Read file
+                    bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+                        outputStream.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    }
+                    outputStream.writeBytes(lineEnd);
+                    outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+                }
+
+                int serverResponseCode = connection.getResponseCode();
+                String serverResponseMessage = connection.getResponseMessage();
+                Log.d("serverResponseCode", "" + serverResponseCode);
+                Log.d("serverResponseMessage", "" + serverResponseMessage);
+
+                fileInputStream.close();
+                outputStream.flush();
+                outputStream.close();
+
+                if (serverResponseCode == 200) {
+                    return "true";
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return "false";
+        }
+
+        @Override
+        protected void onPostExecute(Object result) {
+
+        }
+    }
+
+
+
+    // -----------------------------------------------------------------------
+    // Private
+    // -----------------------------------------------------------------------
+
+    // Simply forwards to an error page, but you could alternatively show your own native screen
+    // or do whatever other kind of error handling you want.
+    private void handleError(int code) {
+        if (code == 404) {
+            TurbolinksSession.getDefault(this)
+                    .activity(this)
+                    .adapter(this)
+                    .restoreWithCachedSnapshot(false)
+                    .view(turbolinksView)
+                    .visit(BASE_URL + "/error");
+        }
+    }
+}
